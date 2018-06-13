cbe63abded9fc36424d1aece7d9c3c59c58465d7
Natali Hreshchyshyn <39494840+nataligreshchyshyn@users.noreply.github.com>
2018-06-13 03:37:14 +0300

    Create carousel.js

---
+++ js/carousel.js
@@ -0,0 +1,10 @@
+renderCarousel(container, products) {
+        let carouselDomString = ''
+        products.forEach(product => {
+            carouselDomString +=
+                `<div class="carousel-item">
+                  <img class="d-block w-100" src="img/products/${product.image}"
+                      alt="${product.title}">
+                </div>`;
+        });
+    }
